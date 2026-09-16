631d6c3b2c6c765b5dda00a538d6f68cdc4f5431
Lopster1 <vova38338@gmail.com>
2026-09-16 13:52:17 +0300

    fix(db): add missing curator_id migration for production

---
+++ backend/alembic/versions/a1f6b8ea04c9_add_curator_id_to_groups.py
@@ -0,0 +1,17 @@
+"""add curator_id to groups"""
+from typing import Sequence, Union
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'a1f6b8ea04c9'
+down_revision = 'drop_rooms_bind_to_teachers'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.add_column('groups', sa.Column('curator_id', sa.Integer(), nullable=True))
+    op.create_foreign_key('fk_groups_teachers', 'groups', 'teachers', ['curator_id'], ['id'], ondelete='SET NULL')
+
+def downgrade():
+    op.drop_constraint('fk_groups_teachers', 'groups', type_='foreignkey')
+    op.drop_column('groups', 'curator_id')
