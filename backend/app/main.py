0de532f3eb616bc48315349b46abc861da5250c5
Lopster1 <vova38338@gmail.com>
2026-09-22 13:48:46 +0300

    Завантаження розкладу для моб додатку

--- backend/app/main.py
+++ backend/app/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origin_regex=r"^https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
